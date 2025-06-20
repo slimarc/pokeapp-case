c1ceeb5642b2babf0153306f248145f61ed9c339
Ricardo <sricardolimaa@gmail.com>
2025-06-19 23:36:42 -0300

    feat: creating methods to get pokemons from the api

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  private baseUrl = 'https://pokeapi.co/api/v2';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPokemonList(offset: number = 0, limit: number = 20) {
+    return this.http.get(`${this.baseUrl}/pokemon?offset=${offset}&limit=${limit}`);
+  }
+  getPokemonListByName(name: string) {
+    return this.http.get(`${this.baseUrl}/pokemon/${name}`);
+  }
+
 }
