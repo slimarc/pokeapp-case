cd5de0e6b390261763178c63647ece4b04ba223b
Ricardo <sricardolimaa@gmail.com>
2025-06-19 23:40:26 -0300

    feat: adding pokemon list response interface

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+export interface PokemonListResponse {
+  count: number;
+  next: string;
+  previous: string | null;
+  results: any[];
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +17,8 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
-  getPokemonList(offset: number = 0, limit: number = 20) {
-    return this.http.get(`${this.baseUrl}/pokemon?offset=${offset}&limit=${limit}`);
+  getPokemonList(offset: number = 0, limit: number = 20): Observable<PokemonListResponse> {
+    return this.http.get<PokemonListResponse>(`${this.baseUrl}/pokemon?offset=${offset}&limit=${limit}`);
   }
   getPokemonListByName(name: string) {
     return this.http.get(`${this.baseUrl}/pokemon/${name}`);
