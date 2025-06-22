1a7d7c817cc41c4cd4e61eda6a1a8156e14d2fb1
Ricardo <sricardolimaa@gmail.com>
2025-06-22 13:55:20 -0300

    feat: create pokemon page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,98 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  LoadingController,
+  IonButton,
+  IonIcon,
+  IonCard,
+  IonCardContent,
+  IonCardTitle,
+  IonCardHeader,
+  IonSpinner
+} from '@ionic/angular/standalone';
+import { PokemonService } from "../services/pokemon.service";
+import { RouterLink } from "@angular/router";
+import { addIcons } from 'ionicons';
+import { arrowBackCircle, arrowForwardCircle, heart, heartOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonIcon,
+    IonCard,
+    IonCardContent,
+    IonCardTitle,
+    IonCardHeader,
+    IonSpinner,
+    RouterLink
+  ],
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit {
+  pokemonId: number = 1;
+  currentPokemon: any = null;
+  isLoading: boolean = false;
+  isFavorite: boolean = false;
+
+  constructor(
+    private pokemonService: PokemonService,
+    private loadingCtrl: LoadingController
+  ) {
+    addIcons({ arrowBackCircle, arrowForwardCircle, heart, heartOutline });
+  }
+
+  ngOnInit() {
+    this.loadPokemon();
+  }
+
+  async loadPokemon() {
+    this.isLoading = true;
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading...',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+
+    this.pokemonService.getPokemonListByName(this.pokemonId.toString()).subscribe({
+      next: (response) => {
+        this.currentPokemon = response;
+        this.isLoading = false;
+        loading.dismiss();
+      },
+      error: (err) => {
+        console.error('Failed to load Pokémon:', err);
+        this.isLoading = false;
+        loading.dismiss();
+      }
+    });
+  }
+
+  nextPokemon() {
+    this.pokemonId++;
+    this.loadPokemon();
+  }
+
+  previousPokemon() {
+    if (this.pokemonId > 1) {
+      this.pokemonId--;
+      this.loadPokemon();
+    }
+  }
+
+  toggleFavorite() {
+    this.isFavorite = !this.isFavorite;
+    console.log('Favorite:', this.isFavorite);
+  }
 }
