5511996710525b9d217bc0760d1e57312e08e795
Ricardo <sricardolimaa@gmail.com>
2025-06-22 17:04:55 -0300

    feat: add detail pokemon page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,81 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonBackButton,
+  IonButtons,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonSpinner,
+  IonChip,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonProgressBar
+} from '@ionic/angular/standalone';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonBackButton,
+    IonButtons,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonSpinner,
+    IonChip,
+    IonLabel,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonProgressBar
+  ]
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  pokemon: any = null;
+  isLoading: boolean = false;
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) {}
 
+  ngOnInit() {
+    this.loadPokemonDetails();
+  }
+
+  loadPokemonDetails() {
+    this.isLoading = true;
+    const pokemonName = this.route.snapshot.paramMap.get('name');
+
+    if (pokemonName) {
+      this.pokemonService.getPokemonListByName(pokemonName).subscribe({
+        next: (response) => {
+          this.pokemon = response;
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.error('Failed to load pokemon details:', err);
+          this.isLoading = false;
+        }
+      });
+    }
+  }
 }
