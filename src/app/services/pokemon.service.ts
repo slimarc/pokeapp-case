c509dd0b01b03202d2557111858570ea99b398c2
Ricardo <sricardolimaa@gmail.com>
2025-06-22 13:42:53 -0300

    refactor: change methods

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,13 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
 
-export interface PokemonListResponse {
-  count: number;
-  next: string;
-  previous: string | null;
-  results: any[];
-}
 
 @Injectable({
   providedIn: 'root'
@@ -17,9 +10,6 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
-  getPokemonList(offset: number = 0, limit: number = 20): Observable<PokemonListResponse> {
-    return this.http.get<PokemonListResponse>(`${this.baseUrl}/pokemon?offset=${offset}&limit=${limit}`);
-  }
   getPokemonListByName(name: string) {
     return this.http.get(`${this.baseUrl}/pokemon/${name}`);
   }
