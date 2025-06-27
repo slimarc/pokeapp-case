69f12a64acd358d1bf429dfcf5ae57e2b2cec1fb
Ricardo <sricardolimaa@gmail.com>
2025-06-27 20:21:25 -0300

    chore: update git ignore

--- src/app/pokedex/pokedex.page.spec.ts
+++ src/app/pokedex/pokedex.page.spec.ts
@@ -1,12 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { PokedexPage } from './pokedex.page';
 
 describe('PokedexPage', () => {
   let component: PokedexPage;
   let fixture: ComponentFixture<PokedexPage>;
 
-  beforeEach(async () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ PokedexPage ],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
+
     fixture = TestBed.createComponent(PokedexPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
